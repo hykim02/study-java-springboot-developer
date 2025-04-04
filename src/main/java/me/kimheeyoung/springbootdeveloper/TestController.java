c727d7132d970ff1f0b0ca450cec7953da0e1a6d
hykim02 <hykim4435@naver.com>
2025-04-04 16:58:51 +0900

    JUnit test code 실습

--- src/main/java/me/kimheeyoung/springbootdeveloper/TestController.java
+++ src/main/java/me/kimheeyoung/springbootdeveloper/TestController.java
@@ -1,12 +1,20 @@
 package me.kimheeyoung.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+
+    @Autowired // TestService 빈 주입
+    TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "test 페이지입니다.";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
