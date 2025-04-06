a201481d7b149d964f40b2f5dccfdd15b282fbeb
hykim02 <hykim4435@naver.com>
2025-04-06 20:18:00 +0900

    [test]spring data JPA 학습test

--- src/main/java/me/kimheeyoung/springbootdeveloper/MemberRepository.java
+++ src/main/java/me/kimheeyoung/springbootdeveloper/MemberRepository.java
@@ -3,7 +3,9 @@ package me.kimheeyoung.springbootdeveloper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Member> findByName(String name);
 }
