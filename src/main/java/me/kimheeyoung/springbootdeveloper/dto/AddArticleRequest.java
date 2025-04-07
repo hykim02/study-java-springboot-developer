f51dcdd8f26f667907b7354cb49b48ea026311a9
hykim02 <hykim4435@naver.com>
2025-04-07 17:37:11 +0900

    [Feat] 블로그 글 조회 api 생성 및 테스트 코드

--- src/main/java/me/kimheeyoung/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/kimheeyoung/springbootdeveloper/dto/AddArticleRequest.java
@@ -13,7 +13,7 @@ public class AddArticleRequest {
     private String title;
     private String content;
 
-    public Article toEntity() { // 생성자를 사용해 객체 생성
+    public Article toEntity() { // 생성자를 사용해 객체 생성 -> 역직렬화
         return Article.builder()
                 .title(title)
                 .content(content)
